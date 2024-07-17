4a0337c24a4f1ff7dbf6170989941ba326aa45fd
carlapvalera <perezvaleracarlas@gmail.com>
2024-07-17 11:40:03 -0400

    protocol code

---
+++ Distributed/helper/protocol_codes.py
@@ -0,0 +1,18 @@
+# Operation codes 
+
+EMPTYBIT=b''
+FIND_SUCCESSOR = 1
+FIND_PREDECESSOR = 2
+#FIND_SUCCESSOR_WITHOUT_PREDECESSOR=10 # Busca el nodo que tiene sucesor y no predecesor el cual es el nodo "0 " por lo cual el nodo de mayor rango de la red debe buscarlo
+GET_SUCCESSOR = 3
+GET_PREDECESSOR = 4
+NOTIFY = 5
+CHECK_PREDECESSOR = 6
+CLOSEST_PRECEDING_FINGER = 7
+STORE_KEY = 8
+RETRIEVE_KEY = 9
+JOIN = 10
+STORE_KEY_CLIENT=11
+FIND_KEY_OWNER=12
+RETRIEVE_KEY_CLIENT=13
+ELECTION=14
