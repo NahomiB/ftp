9faa138c86b12e59d6e55a7a846a68a22c7470c7
Nahomi Bouza <nahomi@divineslns.com>
2024-10-04 00:53:24 -0400

    Delete unused files from previous versions

--- src/Pre_Processing/Document.py
+++
@@ -1,7 +0,0 @@
-DOCUMENTS_UBICATION = "data/documents/"
-DATA_UBICATION = "data/data/"
-class Document:
-    def __init__(self, title : str, text : str):
-        self.title = title
-        self.text = text
-        self.representation = None
--- src/Pre_Processing/Similarity_Matrix.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Tuple
-import gensim
-from Pre_Processing.Document import DATA_UBICATION
-
-def build_similarity_matrix(model : gensim.models.TfidfModel, doc_term_matrix : List[Tuple[int,int]]) -> gensim.similarities.MatrixSimilarity:
-    #sim_matrix = gensim.similarities.MatrixSimilarity(model[doc_term_matrix])
-    sim_matrix = gensim.similarities.MatrixSimilarity(doc_term_matrix)
-    return sim_matrix
